cd32a166b1bf900d67f8b1b6bdee80ca7d75ec1e
davidbridges123 <david.bridges@live.com>
2019-10-26 10:52:16 -0600

    change code

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "David Bridges",
+    "role": "Full Stack Web Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Aspiring and determined web developer. I aim to make a difference through my skills and experience.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/david-bridges-/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/datmellowjello",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitters",
+          "url":"http://twitter.com/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Personable and enthusiastic in the workplace. Quick to learn and adapt to new work environments and programs. Love problem solving with a team. Currently in school for full stack web development.",
+    "address":"Salt Lake City, Utah",
+    "phonenumber":"(470)-419-0851",
+    "education":[
+      
+      {
+        "UniversityName":"U of U",
+        "specialization":"Front-end Web Development",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Advance Medical Designs",
+        "specialization":"Inventory Managment Systems",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2018",
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node"
+      },
+
+    ],
+    "portfolio":[
+      {
+        "name":"Peaches",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"David is the best coder I know!",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"Not hiring David is the same as not liking Taco Bell",
+        "name":"Some  other technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
